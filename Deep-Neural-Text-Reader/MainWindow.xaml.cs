726f060023ade3af4c4c40c8da2fe893c17ca481
michalzaw <mich_z@op.pl>
2017-03-10 23:39:02 +0100

    Added Network class

--- Deep-Neural-Text-Reader/MainWindow.xaml.cs
+++ Deep-Neural-Text-Reader/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Accord.Controls;
+using Accord.Math;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,42 @@ namespace Deep_Neural_Text_Reader
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Network network;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            TestNetwork();
+        }
+
+        public void TestNetwork()
+        {
+            double[][] input = new double[][] {
+                new double[] { 0, 0 },
+                new double[] { 0, 1 },
+                new double[] { 1, 0 },
+                new double[] { 1, 1 }
+            };
+            int[] output = new int[] { 0, 1, 1, 0 };
+
+            network = new Network(2, new[] { 2, 2, 1 });
+            network.iterationCount = 100;
+            network.input = input;
+            network.output = output;
+
+            network.Learn();
+
+            double[] answers = network.CalculateAnswer(network.input).GetColumn(0);
+            int[] answers2 = new int[answers.Length];
+            for (int j = 0; j < answers.Length; ++j)
+            {
+                answers2[j] = (int)Math.Round(answers[j]);
+            }
+
+
+            ScatterplotBox.Show("Expected answers", input, output);
+            ScatterplotBox.Show("Network answers", input, answers2);
         }
     }
 }
---
+++ Deep-Neural-Text-Reader/Network.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Accord.Math;
+using Accord.Neuro.Learning;
+using AForge.Neuro;
+using Accord.Controls;
+
+namespace Deep_Neural_Text_Reader
+{
+    class Network
+    {
+        private IActivationFunction activationFunction;
+        private ActivationNetwork network;
+        private LevenbergMarquardtLearning teacher;
+
+        public int iterationCount { set; get; }
+
+        public double[][] input { set; get; }
+        public int[] output { set; get; }
+
+        public Network(int inputsCount, int[] neuronsCount)
+        {
+            activationFunction = new BipolarSigmoidFunction();
+            network = new ActivationNetwork(activationFunction, inputsCount, neuronsCount);
+            teacher = new LevenbergMarquardtLearning(network)
+            {
+                UseRegularization = false
+            };
+        }
+
+        public void Learn()
+        {
+            double[][] y = output.ToDouble().ToArray();
+
+            double error;
+            for (int i = 0; i < iterationCount; ++i)
+            {
+                error = teacher.RunEpoch(input, y);
+            }
+        }
+        
+        public double[][] CalculateAnswer(double[][] input)
+        {
+            return input.Apply(network.Compute);
+        }
+    }
+}
